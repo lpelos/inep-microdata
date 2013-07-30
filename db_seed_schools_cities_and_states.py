8c5402d03fde0a21cd64135f16323ad0ba18ba6a
Lucas Pélos <lucas.pelos@gmail.com>
2013-07-30 19:56:51 -0300

    refatoração no scriopr de importação de escolas

--- db_seed_schools_cities_and_states.py
+++ db_seed_schools_cities_and_states.py
@@ -7,35 +7,26 @@ with open("static/data/sudeste-estados-cidades-escolas.csv", "r") as f:
     state = last_state_code = None
     city = last_city_code = None
     count = 0
+    total_lines = len(list(f))
+    f.seek(0)
 
+    print("0%..")
     for line in lines:
         state_acronym, city_code, city_name, school_code, school_name = line
         state_code = city_code[0:2]
 
         if state_code is not last_state_code:
-
-            state = State.objects(code=state_code).first()
-            if not state:
-                state = State(code=state_code, acronym=state_acronym)
-                state.save()
-
+            state = State.objects.get_or_create(code=state_code, defaults={"acronym": state_acronym})[0]
             last_state_code = state_code
 
-
         if city_code is not last_city_code:
-
-            city = City.objects(code=city_code).first()
-            if not city:
-                city = City(code=city_code, name=city_name, state=state)
-                city.save()
-
+            city = City.objects.get_or_create(code=city_code, defaults={"name": city_name, "state": state})[0]
             last_city_code = city
 
-
-        school = School.objects(code=school_code).first()
-        if not school:
-            school = School(code=school_code, name=school_name, city=city, state=state)
-            school.save()
+        school = School.objects.get_or_create(code=school_code, defaults={"name": school_name, "city": city, "state": state})[0]
 
         count += 1
-        if count % 1000 is 0: print(count)
+        if count % 1000 is 0:
+            print("..%s%%.." % str(count*100/total_lines))
+
+    print("..100%")
