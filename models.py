3bc2919d00e8867626debd6b2f5ba7ec94b05988
Lucas Pélos <lucas.pelos@gmail.com>
2013-08-02 12:02:36 -0300

    database host

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
+import os
 from mongoengine import *
 
-connect('inep-microdata')
+connect("inep-microdata", host=os.environ.get("MONGOHQ_URL", "localhost"))
 
 class ScoreSheet(EmbeddedDocument):
     fields = DictField()
@@ -8,17 +9,17 @@ class ScoreSheet(EmbeddedDocument):
 class State(Document):
     code = StringField(required=True)
     acronym = StringField(required=True, max_length=50)
-    score_sheets = MapField(EmbeddedDocumentField(ScoreSheet))
+    score_sheets = MapField(field=EmbeddedDocumentField(ScoreSheet))
 
 class City(Document):
     code = StringField(required=True)
     name = StringField(required=True, max_length=50)
     state = ReferenceField(State)
-    score_sheets = MapField(EmbeddedDocumentField(ScoreSheet))
+    score_sheets = MapField(field=EmbeddedDocumentField(ScoreSheet))
 
 class School(Document):
     code = StringField(required=True)
     name = StringField(required=True, max_length=100)
     city = ReferenceField(City)
     state = ReferenceField(State)
-    score_sheets = MapField(EmbeddedDocumentField(ScoreSheet))
+    score_sheets = MapField(field=EmbeddedDocumentField(ScoreSheet))
